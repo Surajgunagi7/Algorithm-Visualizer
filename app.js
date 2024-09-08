8cef773db96e29081327694eccfed55bb887d1fa
Suraj Gunagi <surajgunagi7025@gmail.com>
2024-09-08 22:13:54 +0530

    changes

--- app.js
+++ app.js
@@ -131,7 +131,7 @@ async function InsertionSort(array) {
         while (j >= 0 && array[j] > key) {
             array[j + 1] = array[j];
             bars[j + 1].style.height = array[j + 1] * heightFactor + "px";
-            bars[j + 1].style.backgroundColor = "#9AE975";
+            bars[j + 1].style.backgroundColor = "#6bdd36";
             bars[j + 1].style.color = "#191a22"; 
             bars[j + 1].innerHTML = array[j + 1];
 
@@ -203,51 +203,155 @@ async function SelectionSort(array) {
     return array;
 }
 
-async function QuickSort(items, left = 0, right = items.length - 1) {
-    if (left < right) {
-        let index = await partition(items, left, right);
-        await QuickSort(items, left, index - 1);
-        await QuickSort(items, index, right);
+async function QuickSort(arr, low = 0, high = arr.length - 1) {
+    if (low < high) {
+        let pivotIndex = await partition(arr, low, high);
+        await QuickSort(arr, low, pivotIndex - 1);
+        await QuickSort(arr, pivotIndex + 1, high);
     }
-    return items;
+    return arr;
 }
 
-async function partition(items, left, right) {
-    let pivotIndex = Math.floor((right + left) / 2);
-    let pivot = items[pivotIndex];
-    let i = left, j = right;
+async function partition(array, low, high) {
+    let pivot = array[low];
+    let i = low;
+    let j = high;
     let bars = document.getElementsByClassName("bar");
-    let maxArrayValue = Math.max(...items);
+    let maxArrayValue = Math.max(...array);
     let heightFactor = maxHeight / maxArrayValue;
 
-    while (i <= j) {
-        while (items[i] < pivot) i++;
-        while (items[j] > pivot) j--;
-        if (i <= j) {
-            [items[i], items[j]] = [items[j], items[i]];
+    bars[low].style.backgroundColor = "#FF4949"; // Mark pivot
 
-            bars[i].style.height = items[i] * heightFactor + "px";
-            bars[i].style.backgroundColor = "#9AE975";
-            bars[i].style.color = "#191a22"; 
-            bars[i].innerText = items[i];
+    while (i < j) {
+        while (array[i] <= pivot && i <= high - 1) i++;
+        while (array[j] > pivot && j >= low + 1) j--;
 
-            bars[j].style.height = items[j] * heightFactor + "px";
-            bars[j].style.backgroundColor = "#9AE975";
-            bars[j].style.color = "#191a22"; 
-            bars[j].innerText = items[j];
+        if (i < j) {
+            let temp = array[i];
+            array[i] = array[j];
+            array[j] = temp;
+
+            bars[i].style.height = array[i] * heightFactor + "px";
+            bars[i].style.backgroundColor = "#9AE975"; 
+            bars[i].innerHTML = array[i];
+
+            bars[j].style.height = array[j] * heightFactor + "px";
+            bars[j].style.backgroundColor = "#9AE975"; 
+            bars[j].innerHTML = array[j];
 
             await sleep(speedFactor);
 
-            bars[i].style.backgroundColor = "#191a22";
-            bars[i].style.color = "#f3f3f3"; 
+            bars[i].style.backgroundColor = "#191a22"; 
             bars[j].style.backgroundColor = "#191a22";
-            bars[j].style.color = "#f3f3f3";
-
-            i++;
-            j--;
         }
     }
-    return i;
+
+    let temp = array[low];
+    array[low] = array[j];
+    array[j] = temp;
+
+    bars[low].style.height = array[low] * heightFactor + "px";
+    bars[low].style.backgroundColor = "#191a22"; 
+    bars[low].innerHTML = array[low];
+
+    bars[j].style.height = array[j] * heightFactor + "px";
+    bars[j].style.backgroundColor = "#FF4949"; 
+    bars[j].innerHTML = array[j];
+
+    await sleep(speedFactor);
+
+    bars[j].style.backgroundColor = "#191a22"; 
+
+    return j;
+}
+
+async function MergeSort(array, low = 0, high = array.length - 1) {
+    if (low < high) {
+        let mid = Math.floor(low + (high - low) / 2);
+        await MergeSort(array, low, mid);
+        await MergeSort(array, mid + 1, high);
+        await merge(array, low, mid, high);
+    }
+}
+
+async function merge(array, low, mid, high) {
+    let bars = document.getElementsByClassName("bar");
+    let maxArrayValue = Math.max(...array);
+    let heightFactor = maxHeight / maxArrayValue;
+
+    const temp = new Array(high - low + 1);
+    let left = low;
+    let right = mid + 1;
+    let i = 0;
+
+    for (let j = low; j <= high; j++) {
+        bars[j].style.backgroundColor = "#191a22"; 
+        bars[j].style.color = "#f3f3f3";
+    }
+
+    while (left <= mid && right <= high) {
+        if (array[left] <= array[right]) {
+            temp[i] = array[left];
+
+            bars[left].style.backgroundColor = "#6bdd36"; 
+            bars[left].style.color = "#191a22"; 
+
+            await sleep(speedFactor);
+
+            left++;
+        } else {
+            temp[i] = array[right];
+
+            
+            bars[right].style.backgroundColor = "#ff8c00"; 
+            bars[right].style.color = "#191a22"; 
+
+            await sleep(speedFactor);
+
+            right++;
+        }
+        i++;
+    }
+
+    while (left <= mid) {
+        temp[i] = array[left];
+
+        bars[left].style.backgroundColor = "#6bdd36";
+        bars[left].style.color = "#191a22"; 
+
+        await sleep(speedFactor);
+
+        left++;
+        i++;
+    }
+
+    while (right <= high) {
+        temp[i] = array[right];
+
+        bars[right].style.backgroundColor = "#ff8c00";
+        bars[right].style.color = "#191a22"; 
+
+        await sleep(speedFactor);
+
+        right++;
+        i++;
+    }
+
+    for (let j = low; j <= high; j++) {
+        array[j] = temp[j - low];
+
+        bars[j].style.height = array[j] * heightFactor + "px";
+        bars[j].style.backgroundColor = "#9AE975"; // Sorted color
+        bars[j].style.color = "#191a22";
+        bars[j].innerText = array[j];
+
+        await sleep(speedFactor);
+    }
+    
+    for (let j = low; j <= high; j++) {
+        bars[j].style.backgroundColor = "#191a22";
+        bars[j].style.color = "#f3f3f3";
+    }
 }
 
 sortBtn.addEventListener("click", function () {
@@ -256,6 +360,7 @@ sortBtn.addEventListener("click", function () {
         case "insertion": InsertionSort(unsorted_array); break;
         case "selection": SelectionSort(unsorted_array); break;
         case "quick": QuickSort(unsorted_array); break;
+        case "merge": MergeSort(unsorted_array); break;
         default: BubbleSort(unsorted_array); break;
     }
 });
